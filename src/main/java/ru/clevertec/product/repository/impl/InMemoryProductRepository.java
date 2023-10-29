bcc01815f797cd2db7f16cb299c6dd58c128e12c
Kihtenko Olga <kiok.ko1811@gmail.com>
2023-10-29 07:48:47 -0700

    feat: InMemoryProductRepository, ProductMapperImpl, ProductServiceImpl

--- src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
+++ src/main/java/ru/clevertec/product/repository/impl/InMemoryProductRepository.java
@@ -1,31 +1,42 @@
 package ru.clevertec.product.repository.impl;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import ru.clevertec.product.entity.Product;
 import ru.clevertec.product.repository.ProductRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class InMemoryProductRepository implements ProductRepository {
+
+    private Map<UUID, Product> productBase = new HashMap<>();
 
     @Override
     public Optional<Product> findById(UUID uuid) {
-        return Optional.empty();
+        return Optional.ofNullable(productBase.get(uuid));
     }
 
     @Override
     public List<Product> findAll() {
-        return null;
+        return productBase.values().stream().toList();
     }
 
     @Override
     public Product save(Product product) {
-        return null;
+        if (product.getUuid() == null || !productBase.containsKey(product.getUuid()))
+            product.setUuid(UUID.randomUUID());
+        productBase.put(product.getUuid(), product);
+        return product;
     }
 
     @Override
     public void delete(UUID uuid) {
-
+        productBase.remove(uuid);
     }
 }
--- src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
+++ src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
@@ -3,11 +3,15 @@ package ru.clevertec.product.service.impl;
 import lombok.RequiredArgsConstructor;
 import ru.clevertec.product.data.InfoProductDto;
 import ru.clevertec.product.data.ProductDto;
+import ru.clevertec.product.entity.Product;
+import ru.clevertec.product.exception.ProductNotFoundException;
 import ru.clevertec.product.mapper.ProductMapper;
 import ru.clevertec.product.repository.ProductRepository;
 import ru.clevertec.product.service.ProductService;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -18,22 +22,32 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public InfoProductDto get(UUID uuid) {
-        return null;
+        return mapper.toInfoProductDto(productRepository.findById(uuid)
+                .orElseThrow(() -> new ProductNotFoundException(uuid)));
     }
+
 
     @Override
     public List<InfoProductDto> getAll() {
-        return null;
+        return productRepository.findAll()
+                .stream()
+                .map(mapper::toInfoProductDto)
+                .toList();
     }
 
     @Override
     public UUID create(ProductDto productDto) {
-        return null;
+        Objects.requireNonNull(productDto);
+        Product newProduct = mapper.toProduct(productDto);
+        return productRepository.save(newProduct).getUuid();
     }
 
     @Override
     public void update(UUID uuid, ProductDto productDto) {
-
+        productRepository.findById(uuid)
+                .map(find -> mapper.merge(find, productDto))
+                .flatMap(product -> Optional.ofNullable(productRepository.save(product)))
+                .orElseThrow(() -> new ProductNotFoundException(uuid));
     }
 
     @Override
