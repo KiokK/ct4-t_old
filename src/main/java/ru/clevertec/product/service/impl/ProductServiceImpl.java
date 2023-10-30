1eb8adb97a34a153cce6b39ae62d3b3569251ef3
Kihtenko Olga <kiok.ko1811@gmail.com>
2023-10-30 05:22:34 -0700

    refactor: refactoring impl + tests

--- src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
+++ src/main/java/ru/clevertec/product/service/impl/ProductServiceImpl.java
@@ -5,13 +5,12 @@ import ru.clevertec.product.data.InfoProductDto;
 import ru.clevertec.product.data.ProductDto;
 import ru.clevertec.product.entity.Product;
 import ru.clevertec.product.exception.ProductNotFoundException;
+import ru.clevertec.product.exception.ValidationException;
 import ru.clevertec.product.mapper.ProductMapper;
 import ru.clevertec.product.repository.ProductRepository;
 import ru.clevertec.product.service.ProductService;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.UUID;
 
 @RequiredArgsConstructor
@@ -22,8 +21,9 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public InfoProductDto get(UUID uuid) {
-        return mapper.toInfoProductDto(productRepository.findById(uuid)
-                .orElseThrow(() -> new ProductNotFoundException(uuid)));
+        return productRepository.findById(uuid)
+                .map(mapper::toInfoProductDto)
+                .orElseThrow(() -> new ProductNotFoundException(uuid));
     }
 
 
@@ -37,7 +37,8 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public UUID create(ProductDto productDto) {
-        Objects.requireNonNull(productDto);
+        if (productDto == null)
+            throw new ValidationException(ProductDto.class);
         Product newProduct = mapper.toProduct(productDto);
         return productRepository.save(newProduct).getUuid();
     }
@@ -45,8 +46,8 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void update(UUID uuid, ProductDto productDto) {
         productRepository.findById(uuid)
-                .map(find -> mapper.merge(find, productDto))
-                .flatMap(product -> Optional.ofNullable(productRepository.save(product)))
+                .map(curentProduct -> mapper.merge(curentProduct, productDto))
+                .map(productRepository::save)
                 .orElseThrow(() -> new ProductNotFoundException(uuid));
     }
 
